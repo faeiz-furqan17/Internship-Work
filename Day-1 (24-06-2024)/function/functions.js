3d8c5ffc2b32403302642749cc9df7b56dddac1b
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-06-24 18:22:24 +0500

    functions in js

---
+++ Day-1 (24-06-2024)/function/functions.js
@@ -0,0 +1,28 @@
+a()
+b()
+console.log(x)
+
+var x = 1
+
+a()
+b()
+console.log(x)
+
+
+
+function a(){
+    var x = 10
+    console.log("hi")
+    console.log(x)
+    
+}
+// a()
+function b(){
+    var x = 100
+    console.log("bye")
+    console.log(x)
+
+}
+a()
+b()
+console.log(x)
