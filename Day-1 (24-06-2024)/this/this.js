3d3c6fb966ba0000893b5b02f43102fe6c917642
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-06-24 18:47:37 +0500

    this usage in js

---
+++ Day-1 (24-06-2024)/this/this.js
@@ -0,0 +1,15 @@
+var person = {
+    name: "faeiz",
+    age: 22,
+    intro(){console.log(this.name +"and "+person2.name+"are friends")
+        console.log
+    } 
+}
+
+
+var person2 = {
+    name: "Furqan",
+    age: 48
+}
+
+person.intro()
