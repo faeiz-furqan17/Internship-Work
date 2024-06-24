635e499341eecbacb80208c2d50f0d69e44777c6
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-06-24 17:53:57 +0500

    hoisting

---
+++ Day-1 (24-06-2024)/hoisting in java/hoisting.js
@@ -0,0 +1,17 @@
+console.log(fn)
+fn()
+
+console.log(fn2)
+fn2()
+
+console.log(x)
+
+var x = 7
+function fn (){
+    console.log("hahaha")
+}
+var fn2 = ()=> console.log("i am sad")
+fn()
+fn2()
+console.log(x)
+
