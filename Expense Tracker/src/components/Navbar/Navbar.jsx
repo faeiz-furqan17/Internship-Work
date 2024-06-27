ee58cd823a669841c9e149090c3df5927e4ca00c
faeiz-furqan17 <faeiz.furqan@arbisoft.com>
2024-06-27 12:42:21 +0500

    changed buttons on Navbar

--- Expense Tracker/src/components/Navbar/Navbar.jsx
+++ Expense Tracker/src/components/Navbar/Navbar.jsx
@@ -6,18 +6,24 @@ import styles from './Navbar.module.scss';
 function Navbar() {
   return (
     <div>
-      <div className={styles.navbar}>
-        <div className={styles.logo}>
-          <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4-xjZQP_eKyqr_bXBBoqGfKsmVNdYS8mcw&s" />
+<header>
+        <div className={styles.navbar}>
+          <div className={styles.logo}>
+            <img  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV4-xjZQP_eKyqr_bXBBoqGfKsmVNdYS8mcw&s" />
+          </div>
+          <div className={styles.links}>
+            <ul >
+             <li><button>
+              <a href="">Add</a>
+             </button></li>
+             
+             <li><button>
+              <a href="">Dashboard</a>
+             </button></li>
+            </ul>
+          </div>
         </div>
-        <div className={styles.links}>
-          <ul >
-           <li><a href="">form</a></li>
-           <li><a href="">list</a></li>
-           <li><a href="">summary</a></li>
-          </ul>
-        </div>
-      </div>
+</header>
     </div>
   )
 }
